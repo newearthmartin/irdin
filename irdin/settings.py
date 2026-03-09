d4a69a08c9d778a61393f0158e886beaeb3b4adb
Martin Massera <martinmassera@gmail.com>
2026-03-09 20:19:43 -0300

    Add gunicorn, local_settings support, gitignore local_settings

--- irdin/settings.py
+++ irdin/settings.py
@@ -120,3 +120,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
